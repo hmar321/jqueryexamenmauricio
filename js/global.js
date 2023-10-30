768cc63ad407774c6a3e4ea504058b54d75f2b21
hmar321 <hmauricio.ar@gmail.com>
2023-10-30 13:11:13 +0100

    30/10/2023

--- js/global.js
+++ js/global.js
@@ -1,2 +1,20 @@
-var globalUrl= "";
+var globalUrlApiSeries= "https://apiseriespersonajes.azurewebsites.net/";
 
+function globalPintarSeries(selector) {
+    var request = "api/series";
+    var url = globalUrlApiSeries + request;
+    $.ajax({
+        type: "GET",
+        url: url,
+        contentType: "application/json",
+        success: function (data) {
+            var html = "";
+            $.each(data, function (index, serie) {
+                html += "<li>";
+                html += "<a class='dropdown-item' href='Serie.html?id="+serie.idSerie+"'>"+serie.nombre+"</a>";
+                html += "</li>";
+            });
+            $(selector).html(html);
+        }
+    });
+}
